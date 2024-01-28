fe0a0c44ab64c8720004ab274867e4f01abb7d45
dDevusS <geckahilden@gmail.com>
2024-01-28 00:13:52 -0500

    Reorganazed PlayerDAOTest. Added display names.

--- src/test/java/com/ddevuss/tennisScoreboard/DAO/PlayerDAOTest.java
+++ src/test/java/com/ddevuss/tennisScoreboard/DAO/PlayerDAOTest.java
@@ -9,49 +9,64 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Tag("DAOImplementedClass")
 class PlayerDAOTest {
 
     private static Player player1;
+    private static Player player2;
     private static PlayerDAO playerDAO;
 
     @BeforeAll
     static void createDB() {
         player1 = Player.of().name("Alex").build();
+        player2 = Player.of().name("Max").build();
         playerDAO = new PlayerDAO();
         }
 
     @Test
     @Order(1)
-    void saveMethodReturnsPlayerWithId() {
+    @DisplayName("will be true if it returns same player with ID")
+    void saveMethodReturnsSamePlayerWithId() {
         var newPlayer = playerDAO.save(player1);
+        playerDAO.save(player2);
 
-        assertThat(newPlayer).isNotNull();
-        assertThat(player1.getName()).as(newPlayer.getName());
         assertThat(newPlayer.getId()).isNotZero();
+        assertThat(newPlayer).isEqualTo(player1);
     }
 
-    @Test
+    @Nested
     @Order(2)
-    void findById() {
-        Integer correctId = 1;
-        Integer incorrectId = 100;
+    class FindByIdTest {
 
-        var player = playerDAO.findById(correctId);
-        assertThat(player).isNotNull();
+        @Test
+        @DisplayName("will be true if player exists with this ID")
+        void returnPlayerClassWithCorrectId() {
+            Integer correctId = 1;
+            var player = playerDAO.findById(correctId);
+            assertThat(player).isInstanceOf(Player.class);
+        }
 
-        var mustBeNull = playerDAO.findById(incorrectId);
-        assertThat(mustBeNull).isNull();
+        @Test
+        @DisplayName("will be false if player doesn't exist with this ID")
+        void returnNullWithIncorrectId() {
+            Integer incorrectId = 100;
+            var mustBeNull = playerDAO.findById(incorrectId);
+            assertThat(mustBeNull).isNull();
+        }
+
     }
 
     @Test
     @Order(3)
+    @DisplayName("Compare size of players list with number of added players by previous test")
     void getPlayersListWithSizeEqualedOne() {
         var players = playerDAO.findAll();
-        assertThat(players).size().isEqualTo(1);
+        assertThat(players).size().isEqualTo(2);
     }
 
     @Test
     @Order(4)
+    @DisplayName("will be true if method returns updated name")
     void updatedPlayerHasSameName() {
         String newName = "Sergey";
         player1.setName(newName);
@@ -64,19 +79,25 @@ class PlayerDAOTest {
         assertThat(updatedPlayer.getName()).isEqualTo(newName);
     }
 
-    @Test
+    @Nested
     @Order(5)
-    void deleteByNonExistedIdPlayerReturnFalse() {
-        Integer unrealId = 1000;
-        boolean isDeleted = playerDAO.delete(unrealId);
-        assertThat(isDeleted).isFalse();
-    }
+    class DeleteTest {
 
-    @Test
-    @Order(6)
-    void deleteExistedPlayerByIdReturnTrue() {
-        boolean isDeleted = playerDAO.delete(player1.getId());
-        assertThat(isDeleted).isTrue();
+        @Test
+        @DisplayName("will be false if player with this ID doesn't exist in the DB")
+        void deleteByNonExistedIdPlayerReturnFalse() {
+            Integer unrealId = 1000;
+            boolean isDeleted = playerDAO.delete(unrealId);
+            assertThat(isDeleted).isFalse();
+        }
+
+        @Test
+        @DisplayName("will be true if player with this ID exists in the DB")
+        void deleteExistedPlayerByIdReturnTrue() {
+            boolean isDeleted = playerDAO.delete(player2.getId());
+            assertThat(isDeleted).isTrue();
+        }
+
     }
 
 }
