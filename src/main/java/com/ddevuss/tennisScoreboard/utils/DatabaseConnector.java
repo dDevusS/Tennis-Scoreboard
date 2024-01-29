d1e38c9a7f7b0574a8058351d0daead69a95babf
dDevusS <geckahilden@gmail.com>
2024-01-28 21:27:44 -0500

    Added Index for column name of table Players in the DB

--- src/main/java/com/ddevuss/tennisScoreboard/utils/DatabaseConnector.java
+++ src/main/java/com/ddevuss/tennisScoreboard/utils/DatabaseConnector.java
@@ -20,10 +20,12 @@ public class DatabaseConnector {
                                                        "FOREIGN KEY (Player1) REFERENCES Players(ID) ON DELETE CASCADE," +
                                                        "FOREIGN KEY (Player2) REFERENCES Players(ID) ON DELETE CASCADE," +
                                                        "FOREIGN KEY (Winner) REFERENCES Players(ID) ON DELETE CASCADE);";
+    private static final String CREATE_INDEX_FOR_PLAYERS_NAME = "CREATE INDEX ind_name ON Players (Name);";
 
     private DatabaseConnector() {
-        createTable(CREATE_PLAYERS_TABLE);
-        createTable(CREATE_MATCHES_TABLE);
+        createSQLRequest(CREATE_PLAYERS_TABLE);
+        createSQLRequest(CREATE_INDEX_FOR_PLAYERS_NAME);
+        createSQLRequest(CREATE_MATCHES_TABLE);
     }
 
     public Session getSession() {
@@ -31,7 +33,7 @@ public class DatabaseConnector {
         return sessionFactory.openSession();
     }
 
-    private void createTable(String sqlString) {
+    private void createSQLRequest(String sqlString) {
         try (var session = getSession()) {
             session.beginTransaction();
             session.createNativeMutationQuery(sqlString).executeUpdate();
