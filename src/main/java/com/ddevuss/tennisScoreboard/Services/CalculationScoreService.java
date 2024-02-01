c19e9c068e6902abbc139c09d62ad06f60706614
dDevusS <geckahilden@gmail.com>
2024-01-31 23:46:52 -0500

    Finished realization logic for CalculationScoreService

--- src/main/java/com/ddevuss/tennisScoreboard/Services/CalculationScoreService.java
+++ src/main/java/com/ddevuss/tennisScoreboard/Services/CalculationScoreService.java
@@ -6,80 +6,150 @@ import lombok.Getter;
 
 import java.util.UUID;
 
-public class ScoreCalculationService {
+public class CalculationScoreService {
 
     @Getter
-    private static final ScoreCalculationService INSTANCE = new ScoreCalculationService();
+    private static final CalculationScoreService INSTANCE = new CalculationScoreService();
     private final MainMatchesService mainMatchesService = MainMatchesService.getINSTANCE();
     private final FinishedMatchPersistenceService finishedMatchPersistenceService
             = FinishedMatchPersistenceService.getINSTANCE();
 
-    private ScoreCalculationService() {
+    private CalculationScoreService() {
     }
 
     public void plusPointToPlayer(UUID uuid, int playerId) {
         CurrentMatch currentMatch = mainMatchesService.getCurrentMatches().get(uuid);
-        plusPointToActivePlayer(currentMatch, playerId);
-        checkEndOfSet(currentMatch);
-    }
 
-    private void checkEndOfSet(CurrentMatch currentMatch) {
-        ActivePlayer activePlayer1 = currentMatch.getPlayer1();
-        ActivePlayer activePlayer2 = currentMatch.getPlayer2();
-
-        if (currentMatch.isTieBreak()
-                && Math.abs(activePlayer1.getScore() - activePlayer2.getScore()) >= 8) {
-
-            if (activePlayer1.getScore() - activePlayer2.getScore() > 0) {
-                resetScoreAndPlusSet(activePlayer1, activePlayer2);
-            }
-            else {
-                resetScoreAndPlusSet(activePlayer2, activePlayer1);
-            }
+        if (currentMatch.isDeuce()) {
+            decideDeuceSituation(currentMatch, playerId);
+            checkEndOfSet(currentMatch);
+        }
+        else if (currentMatch.isTieBreak()) {
+            plusPointToActivePlayer(currentMatch, playerId);
+            checkEndOfSetIfTieBreak(currentMatch);
         }
         else {
-            if (activePlayer1.getScore() / 4 >= 6 && activePlayer1.getScore() / 4 - activePlayer2.getScore() / 4 >= 2) {
-                resetScoreAndPlusSet(activePlayer1, activePlayer2);
-            }
-            else if (activePlayer2.getScore() / 4 >= 6 && activePlayer2.getScore() / 4 - activePlayer1.getScore() / 4 >= 2) {
-                resetScoreAndPlusSet(activePlayer2, activePlayer1);
-            }
-            else if (activePlayer1.getScore() >= 24 || activePlayer2.getScore() >= 24) {
-                currentMatch.setTieBreak(true);
-            }
+            plusPointToActivePlayer(currentMatch, playerId);
+            checkEndOfGame(currentMatch, playerId);
+            checkEndOfSet(currentMatch);
         }
 
         checkEndOfMatch(currentMatch);
     }
 
-    private void plusPointToActivePlayer(CurrentMatch currentMatch, int playerId) {
-        int victoryPoint = 1;
-
-        if (currentMatch.isTieBreak()) {
-            victoryPoint = 4;
+    private void decideDeuceSituation(CurrentMatch currentMatch, int playerId) {
+        if (currentMatch.getPlayer1().getId() == playerId && currentMatch.getPlayer1().isAdvantage()) {
+            resetScoreAndPlusGame(currentMatch.getPlayer1(), currentMatch.getPlayer2());
         }
-
-        if (currentMatch.getPlayer1().getId() == playerId) {
-            int newScore = currentMatch.getPlayer1().getScore() + victoryPoint;
-            currentMatch.getPlayer1().setScore(newScore);
+        else if (currentMatch.getPlayer1().getId() == playerId) {
+            currentMatch.getPlayer1().setAdvantage(true);
+            currentMatch.getPlayer2().setAdvantage(false);
+        }
+        else if (currentMatch.getPlayer2().isAdvantage()) {
+            resetScoreAndPlusGame(currentMatch.getPlayer2(), currentMatch.getPlayer1());
         }
         else {
-            int newScore = currentMatch.getPlayer2().getScore() + victoryPoint;
-            currentMatch.getPlayer2().setScore(newScore);
+            currentMatch.getPlayer2().setAdvantage(true);
+            currentMatch.getPlayer1().setAdvantage(false);
+        }
+    }
+
+    private void checkEndOfSetIfTieBreak(CurrentMatch currentMatch) {
+        ActivePlayer activePlayer1 = currentMatch.getPlayer1();
+        ActivePlayer activePlayer2 = currentMatch.getPlayer2();
+        int tieBreakBound = 7;
+
+        if (activePlayer1.getScore() >= tieBreakBound
+                && activePlayer1.getScore() - activePlayer2.getScore() > 1) {
+
+            resetScoreAndPlusSet(activePlayer1, activePlayer2);
+            currentMatch.setTieBreak(false);
+        }
+        else if (activePlayer2.getScore() >= tieBreakBound
+                && activePlayer2.getScore() - activePlayer1.getScore() > 1) {
+
+            resetScoreAndPlusSet(activePlayer2, activePlayer1);
+            currentMatch.setTieBreak(false);
+        }
+    }
+
+    private void  checkEndOfGame(CurrentMatch currentMatch, int playerId) {
+        ActivePlayer activePlayer1 = currentMatch.getPlayer1();
+        ActivePlayer activePlayer2 = currentMatch.getPlayer2();
+        int fortyPoints = 3;
+        int scoreLimitForGame = 4;
+
+        if (activePlayer1.getScore() == scoreLimitForGame) {
+            resetScoreAndPlusGame(activePlayer1, activePlayer2);
+        }
+        else if (activePlayer2.getScore() == scoreLimitForGame) {
+            resetScoreAndPlusGame(activePlayer2, activePlayer1);
+        }
+        else if (activePlayer1.getScore() == fortyPoints && activePlayer2.getScore() == fortyPoints) {
+            currentMatch.setDeuce(true);
+
+            if (activePlayer1.getId() == playerId) {
+                activePlayer1.setAdvantage(true);
+            }
+            else {
+                activePlayer2.setAdvantage(true);
+            }
+        }
+    }
+
+    private void checkEndOfSet(CurrentMatch currentMatch) {
+        ActivePlayer activePlayer1 = currentMatch.getPlayer1();
+        ActivePlayer activePlayer2 = currentMatch.getPlayer2();
+        int gamesBound = 6;
+
+        if (activePlayer1.getGame() >= gamesBound || activePlayer2.getGame() >= gamesBound) {
+
+            if (Math.abs(activePlayer1.getGame() - activePlayer2.getGame()) > 1
+                    && activePlayer1.getGame() > activePlayer2.getGame()) {
+
+                resetScoreAndPlusSet(activePlayer1, activePlayer2);
+            }
+            else if (Math.abs(activePlayer1.getGame() - activePlayer2.getGame()) > 1
+                    && activePlayer2.getGame() > activePlayer1.getGame()) {
+
+                resetScoreAndPlusSet(activePlayer2, activePlayer1);
+            }
+            else if (activePlayer1.getGame() == gamesBound && activePlayer2.getGame() == gamesBound){
+                currentMatch.setTieBreak(true);
+            }
+        }
+    }
+
+    private void plusPointToActivePlayer(CurrentMatch currentMatch, int playerId) {
+        if (currentMatch.getPlayer1().getId() == playerId) {
+            currentMatch.getPlayer1().plusScore();
+        }
+        else {
+            currentMatch.getPlayer2().plusScore();
         }
     }
 
     private void checkEndOfMatch(CurrentMatch currentMatch) {
-        int countOfSets = currentMatch.getPlayer1().getSet() + currentMatch.getPlayer2().getSet();
-        if (countOfSets == 3) {
+        int requiredSetsForVictory = 2;
+
+        if (currentMatch.getPlayer1().getSet() == requiredSetsForVictory
+                || currentMatch.getPlayer2().getSet() == requiredSetsForVictory) {
             finishedMatchPersistenceService.finishCurrentMatch(currentMatch);
         }
     }
 
     private void resetScoreAndPlusSet(ActivePlayer wonPlayer, ActivePlayer beatenPlayer) {
         wonPlayer.setScore(0);
+        wonPlayer.setGame(0);
+        wonPlayer.plusSet();
         beatenPlayer.setScore(0);
-        int set = wonPlayer.getSet() + 1;
-        wonPlayer.setSet(set);
+        beatenPlayer.setGame(0);
+    }
+
+    private void resetScoreAndPlusGame(ActivePlayer wonPlayer, ActivePlayer beatenPlayer) {
+        wonPlayer.setScore(0);
+        wonPlayer.plusGame();
+        wonPlayer.setAdvantage(false);
+        beatenPlayer.setScore(0);
     }
 }
