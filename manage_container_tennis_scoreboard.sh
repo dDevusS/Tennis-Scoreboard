b9723fb3c4c7aff0a95d99e936e25b29032d56a6
dDevusS <evgenii@kashin@gmail.com>
2024-02-28 17:30:56 -0500

    Removed create_and_run_contaner.sh => Added new bash script for managing Tennis_Scoreboard's container.

---
+++ manage_container_tennis_scoreboard.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+PROJECT_NAME="Tennis-Scoreboard"
+PROJECT_VERSION="1.0"
+DOCKER_IMAGE="ddevuss/tennis-scoreboard:$PROJECT_VERSION"
+CONTAINER_NAME="tennis_scoreboard_container"
+
+# Функция для проверки существования Docker образа
+check_docker_image() {
+    local image_exists=$(docker images -q $DOCKER_IMAGE)
+    if [ -n "$image_exists" ]; then
+        echo "Docker image \"$DOCKER_IMAGE\" already exists."
+    else
+        echo "Docker image \"$DOCKER_IMAGE\" does not exist. Building the image..."
+        docker build -t $DOCKER_IMAGE ./$PROJECT_NAME/ || { echo "Error: Failed to build Docker image."; exit 1; }
+    fi
+}
+
+# Функция для запуска Docker контейнера
+run_docker_container() {
+    local port=$1
+
+    # Проверка существования контейнера и его перезапуск, если уже существует
+    if docker ps -a --format '{{.Names}}' | grep -Eq "^$CONTAINER_NAME$"; then
+        echo "Container \"$CONTAINER_NAME\" already exists. Restarting the container on port $port..."
+        docker container stop $CONTAINER_NAME
+        docker container rm $CONTAINER_NAME
+    fi
+
+    echo "Running Docker container on port $port..."
+    docker run -d -p $port:8080 --name $CONTAINER_NAME $DOCKER_IMAGE || { echo "Error: Failed to start Docker container."; exit 1; }
+}
+
+# Функция для остановки и удаления Docker контейнера
+stop_docker_container() {
+    if docker ps -a --format '{{.Names}}' | grep -Eq "^$CONTAINER_NAME$"; then
+        echo "Stopping and removing Docker container \"$CONTAINER_NAME\"..."
+        docker container stop $CONTAINER_NAME
+        docker container rm $CONTAINER_NAME
+    else
+        echo "Container \"$CONTAINER_NAME\" does not exist."
+    fi
+}
+
+# Функция для вывода справки
+show_help() {
+    echo "Usage: $0 {install|run [--port PORT_NUMBER]|stop}"
+    echo "Options:"
+    echo "  install        Install or build Docker image"
+    echo "  run            Run Docker container on default port (8080) on PORT_NUMBER with --port PORT_NUMBER"
+    echo "  --port         Specify port number"
+    echo "  stop           Stop and remove Docker container"
+}
+
+# Проверка аргументов командной строки
+if [ $# -eq 1 ]; then
+    case $1 in
+        "install")
+            check_docker_image
+            ;;
+        "run")
+            run_docker_container 8080
+            ;;
+        "stop")
+            stop_docker_container
+            ;;
+        "--help")
+            show_help
+            ;;
+        *)
+            echo "Invalid argument. Use '--help' for usage instructions."
+            exit 1
+            ;;
+    esac
+    elif [ $# -eq 3 ] && [ $1 = "run" ] && [ $2 = "--port" ]; then
+        port=$3
+        if [ "$port" -eq "$port" ] 2>/dev/null; then
+            run_docker_container $port
+        else
+            echo "Invalid port number."
+            exit 1
+        fi
+    else
+        echo "Invalid number of arguments. Use '--help' for usage instructions."
+        exit 1
+    fi
